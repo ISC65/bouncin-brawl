28a6afb05a3af46b5303cf9b8e59013710369461
ISC65 <shonajns22@googlemail.com>
2026-04-25 17:50:42 +0100

    i need to handle some syntax errors for the other files.

---
+++ scripts/classes.js
@@ -0,0 +1,15 @@
+export class Rectangle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+export class Vec2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
